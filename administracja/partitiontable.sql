151e57dd403f261e40fe1a3f239ecd21005a02f7
marcinpytlik <77489303+marcinpytlik@users.noreply.github.com>
2023-04-13 15:14:08 +0200

    nowe skrypty

--- administracja/partitiontable.sql
+++ administracja/partitiontable.sql
@@ -103,3 +103,63 @@ CREATE TABLE dbo.RecipeHistory
 		REFERENCES dbo.RecipeHistoryStatus (RecipeHistoryStatusID)
 )
 ON RecipeHistRange (DateCreated);
+---------------
+-- lookup
+USE Menu;
+GO
+
+SELECT tbl.[name] AS TableName,
+	sch.[name] AS PartitionScheme,
+	fnc.[name] AS PartitionFunction,
+	prt.partition_number,
+	fnc.[type_desc],
+	rng.boundary_id,
+	rng.[value] AS BoundaryValue,
+	prt.[rows]
+FROM sys.tables tbl
+	INNER JOIN sys.indexes idx
+	ON tbl.[object_id] = idx.[object_id]
+	INNER JOIN sys.partitions prt
+	ON idx.[object_id] = prt.[object_id]
+	AND idx.index_id = prt.index_id
+	INNER JOIN sys.partition_schemes AS sch
+	ON idx.data_space_id = sch.data_space_id
+	INNER JOIN sys.partition_functions AS fnc
+	ON sch.function_id = fnc.function_id
+	LEFT JOIN sys.partition_range_values AS rng
+	ON fnc.function_id = rng.function_id
+	AND rng.boundary_id = prt.partition_number
+WHERE tbl.[name] = 'RecipeHistory'
+	AND idx.[type] <= 1
+ORDER BY prt.partition_number; 
+-- sum 
+USE Menu;
+GO
+
+SELECT
+	SUM(
+		CASE WHEN DateCreated < '1/1/2019'
+			THEN 1
+			ELSE 0
+		END
+	) AS Partition1,
+	SUM(
+		CASE WHEN DateCreated < '4/1/2019'
+			THEN 1
+			ELSE 0
+		END
+	) AS Partition2,
+	SUM(
+		CASE WHEN DateCreated < '7/1/2019'
+			THEN 1
+			ELSE 0
+		END
+	) AS Partition3,
+	SUM(
+		CASE WHEN DateCreated >= '7/1/2019'
+			THEN 1
+			ELSE 0
+		END
+	) AS Partition4
+FROM dbo.RecipeHistory;
+-- partition existing table
